551c8222dbc365a226901b7e57f2df52ed464a9c
Evotianus <evotianus0110@gmail.com>
2023-03-06 10:02:30 +0700

    Navbar & Homepage Responsive

--- script.js
+++ script.js
@@ -14,6 +14,22 @@ setTimeout(() => {
         delete activeSlide.dataset.active
     }, 5000)
 
+    // Homepage - Menu Button
+    const menuIcon = document.querySelector(".menu-icon");
+    const menuIconList = document.querySelector(".menu-icon-list")
+
+    menuIcon.addEventListener('click', () => {
+        if (menuIcon.dataset.active) {
+            console.log("ada")
+            menuIconList.style.top = "-1000px"
+            delete menuIcon.dataset.active
+        }
+        else {
+            console.log("ga ada")
+            menuIconList.style.top = "0"
+            menuIcon.dataset.active = true
+        }
+    })
 
     // Maps - Modal / Pop up
     const openModalButtons = document.querySelectorAll("[data-modal-target]")
